b1f8d114261b91a872128e91f620e19a093d29b2
Martijn Vermaat <martijn@vermaat.name>
2013-03-20 15:54:07 +0100

    Test sample collection resource

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -7,6 +7,7 @@ import datetime
 
 from mock import Mock
 from nose.tools import *
+import requests
 
 from manwe import resources, session
 
@@ -16,7 +17,7 @@ class TestResources():
     Test :mod:`manwe.resources`.
     """
     def setup(self):
-        self.session = Mock(session.Session)
+        self.session = Mock(session.Session, uris={'samples': '/samples'})
 
     def test_read_sample(self):
         """
@@ -84,3 +85,38 @@ class TestResources():
             '/samples/3', method='PATCH', data={'name': 'edited test sample',
                                                 'pool_size': 3})
         assert not sample.dirty
+
+    def test_sample_collection(self):
+        """
+        Iterate over the samples in a sample collection.
+        """
+        def create_sample(i):
+            return {'name': 'test sample %i' % i,
+                    'pool_size': 5,
+                    'coverage_profile': True,
+                    'public': False,
+                    'uri': '/samples/%i' % i,
+                    'user_uri': '/users/8',
+                    'added': '2012-11-23T10:55:12'}
+        def create_mock_response(start, end, total):
+            samples = [create_sample(i) for i in range(start, end)]
+            mock_response = Mock(requests.Response)
+            mock_response.json.return_value = {'samples': samples}
+            mock_response.headers = {'Content-Range': 'items %d-%d/%d' % (start, end, total)}
+            return mock_response
+
+        # Total number of samples in our collection is 2 times the cache size
+        # plus 3.
+        total = 2 * resources.COLLECTION_CACHE_SIZE + 3
+        pages = [(1, resources.COLLECTION_CACHE_SIZE + 1),
+                 (resources.COLLECTION_CACHE_SIZE + 1, 2 * resources.COLLECTION_CACHE_SIZE + 1),
+                 (2 * resources.COLLECTION_CACHE_SIZE + 1, 2 * resources.COLLECTION_CACHE_SIZE + 4)]
+        self.session.get.side_effect = (create_mock_response(start, end, total)
+                                        for start, end in pages)
+
+        samples = resources.SampleCollection(self.session)
+        assert_equal(samples.size, total)
+        sample_list = list(samples)
+        assert_equal(len(sample_list), total)
+        assert_equal(sample_list[0].name, 'test sample 1')
+        assert_equal(sample_list[-1].name, 'test sample %i' % total)
