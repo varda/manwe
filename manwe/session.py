3a8d4b0e9df8be0dbe5dab311062c86c6be472a4
Martijn Vermaat <martijn@vermaat.name>
2015-08-31 22:02:46 +0200

    Group resources

--- manwe/session.py
+++ manwe/session.py
@@ -21,7 +21,7 @@ from .errors import (ApiError, BadRequestError, ForbiddenError,
 from . import resources
 
 
-ACCEPT_VERSION = '>=1.0.0,<2.0.0'
+ACCEPT_VERSION = '>=1.1.0,<2.0.0'
 
 
 logger = logging.getLogger('manwe')
@@ -49,6 +49,7 @@ class Session(object):
     _collections = {c.key: c for c in (resources.AnnotationCollection,
                                        resources.CoverageCollection,
                                        resources.DataSourceCollection,
+                                       resources.GroupCollection,
                                        resources.SampleCollection,
                                        resources.UserCollection,
                                        resources.VariantCollection,
