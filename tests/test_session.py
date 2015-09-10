f390a8bb4b4b90bff1b0b63676e06101237c3ad6
Martijn Vermaat <martijn@vermaat.name>
2015-09-10 17:16:06 +0200

    Test filtering collections

--- tests/test_session.py
+++ tests/test_session.py
@@ -3,6 +3,10 @@ Unit tests for :mod:`manwe.session`.
 """
 
 
+import pytest
+import varda
+import varda.models
+
 import utils
 
 
@@ -32,3 +36,43 @@ class TestSession(utils.TestEnvironment):
 
         data_source_uri = self.uri_for_data_source(name='test data source')
         assert data_source.uri == data_source_uri
+
+    def test_samples_by_user(self):
+        """
+        Filter sample collection by user.
+        """
+        a = varda.models.User('User A', 'a')
+        b = varda.models.User('User B', 'b')
+
+        samples_a = [varda.models.Sample(a, 'Sample A %d' % i)
+                     for i in range(50)]
+        samples_b = [varda.models.Sample(b, 'Sample B %d' % i)
+                     for i in range(50)]
+
+        varda.db.session.add_all(samples_a + samples_b)
+        varda.db.session.commit()
+
+        admin = self.session.user(self.uri_for_user(name='Administrator'))
+        a = self.session.user(self.uri_for_user(name='User A'))
+        b = self.session.user(self.uri_for_user(name='User B'))
+
+        samples = self.session.samples()
+        assert samples.size == 100
+
+        samples_a = self.session.samples(user=a)
+        assert samples_a.size == 50
+        assert samples_a.user == a
+        assert next(samples_a).user == a
+        assert next(samples_a).name.startswith('Sample A ')
+
+        samples_b = self.session.samples(user=b)
+        assert samples_b.size == 50
+        assert samples_b.user == b
+        assert next(samples_b).user == b
+        assert next(samples_b).name.startswith('Sample B ')
+
+        samples_admin = self.session.samples(user=admin)
+        assert samples_admin.size == 0
+        assert samples_admin.user == admin
+        with pytest.raises(StopIteration):
+            next(samples_admin)
